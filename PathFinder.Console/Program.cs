4f50021d6254899e120fe9f0af11495fc05871dc
KestutisB <kestutis.bagdonas@frontit.dk>
2020-05-15 22:02:04 +0300

    adding console app again for debugging

---
+++ PathFinder.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PathFinder.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new PathFinder().Find(new int[] { 1, -1, 2, -1, 0, 2, 0  });
+        }
+    }
+}
