2346283de6aaee92798e8388acadad7bfa542fe8
KestutisB <kestutis.bagdonas@frontit.dk>
2020-05-16 23:16:23 +0300

    apply migrations on app start

--- PathFinder.Api/Program.cs
+++ PathFinder.Api/Program.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PathFinder.Api.Data;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PathFinder.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<PathFinderContext>();
+                var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
+
+                if (pendingMigrations.Any())
+                {
+                    await context.Database.MigrateAsync();
+                }
+            }
+
+            await host.StartAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -17,4 +35,5 @@ namespace PathFinder.Api
                     webBuilder.UseStartup<Startup>();
                 });
     }
+
 }
